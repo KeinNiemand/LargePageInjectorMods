5dfa354385a6033c605439ebc1cb621e977db25b
KeinNiemand <18308201+KeinNiemand@users.noreply.github.com>
2024-02-25 11:50:47 +0100

    Moved IO redirection back to PerfomHooks.cpp

--- MiMallocReplacer/PerformHooks.cpp
+++ MiMallocReplacer/PerformHooks.cpp
@@ -136,60 +136,65 @@ void HookAllMallocFunctions(const std::string& ModuleName) {
 }
 
 
-//void RedirectOutputToInjectorPipe() {
-//	// Connect to the named pipe
-//	HANDLE pipe = CreateFile(
-//		L"\\\\.\\pipe\\myoutputpipe",
-//		GENERIC_WRITE,
-//		0, // no sharing
-//		NULL, // default security attributes
-//		OPEN_EXISTING,
-//		0, // default attributes
-//		NULL); // no template file
-//
-//	if (pipe == INVALID_HANDLE_VALUE) {
-//		std::cout << "MiMallocReplacer.dll: Failed to connect to pipe" << std::endl;
-//	}
-//
-//	FreeConsole();
-//	AttachConsole(ATTACH_PARENT_PROCESS);
-//	
-//
-//	FILE* fpio = new FILE();
-//	if (GetStdHandle(STD_OUTPUT_HANDLE) != INVALID_HANDLE_VALUE) {
-//		if (freopen_s(&fpio, "CONOUT$", "w", stdout) != NULL) {
-//			fflush(stdout);
-//		}
-//	}
-//
-//	//RedirectIO(fpio, pipe);
-//
-//	FILE* fperr = new FILE();
-//	if (GetStdHandle(STD_ERROR_HANDLE) != INVALID_HANDLE_VALUE) {
-//		if (freopen_s(&fperr, "CONOUT$", "w", stderr) != NULL) {
-//			fflush(stdout);
-//		}
-//	}
-//
-//	SetStdHandle(STD_OUTPUT_HANDLE, pipe);
-//	SetStdHandle(STD_ERROR_HANDLE, pipe);
-//	RedirectIO(fpio, pipe);
-//	RedirectIO(fperr, pipe);
-//	RedirectIO(stdout, pipe);
-//	RedirectIO(stderr, pipe);
-//}
+void RedirectIO(FILE* hFrom, HANDLE hTo)
+{
+	int fd = _open_osfhandle((intptr_t)hTo, _O_WRONLY | _O_TEXT);
+	_dup2(fd, _fileno(hFrom));
+	setvbuf(hFrom, NULL, _IONBF, 0); //Disable buffering.
+}
+
+void RedirectOutputToInjectorPipe() {
+	// Connect to the named pipe
+	HANDLE pipe = CreateFile(
+		L"\\\\.\\pipe\\myoutputpipe",
+		GENERIC_WRITE,
+		0, // no sharing
+		NULL, // default security attributes
+		OPEN_EXISTING,
+		0, // default attributes
+		NULL); // no template file
+
+	if (pipe == INVALID_HANDLE_VALUE) {
+		std::cout << "MiMallocReplacer.dll: Failed to connect to pipe" << std::endl;
+	}
+
+	FreeConsole();
+	AttachConsole(ATTACH_PARENT_PROCESS);
+
+
+	FILE* fpio = new FILE();
+	if (GetStdHandle(STD_OUTPUT_HANDLE) != INVALID_HANDLE_VALUE) {
+		if (freopen_s(&fpio, "CONOUT$", "w", stdout) != NULL) {
+			fflush(stdout);
+		}
+	}
+
+	//RedirectIO(fpio, pipe);
+
+	FILE* fperr = new FILE();
+	if (GetStdHandle(STD_ERROR_HANDLE) != INVALID_HANDLE_VALUE) {
+		if (freopen_s(&fperr, "CONOUT$", "w", stderr) != NULL) {
+			fflush(stdout);
+		}
+	}
+
+	SetStdHandle(STD_OUTPUT_HANDLE, pipe);
+	SetStdHandle(STD_ERROR_HANDLE, pipe);
+	RedirectIO(fpio, pipe);
+	RedirectIO(fperr, pipe);
+	RedirectIO(stdout, pipe);
+	RedirectIO(stderr, pipe);
+}
 
 extern "C" void __stdcall NativeInjectionEntryPoint(REMOTE_ENTRY_INFO* inRemoteInfo)
 {
-	/*RedirectOutputToInjectorPipe();*/;
+	
 	
 #ifdef DEBUG
 	Sleep(10000);
 #endif
-
-	//mi_version() to intilse mi malloc.
-	//mi_version();
-
+	mi_version();
+	RedirectOutputToInjectorPipe();
 	
 #ifdef DEBUG
 	std::cout << "MiMallocReplacer.dll: Injected, Trying to Perform Hooks for malloc functions" << std::endl;
